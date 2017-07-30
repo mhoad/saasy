3baeed0e2f6c67d59c5c5795e118e1622b71011a
Mark Hoad <markdhoad@gmail.com>
2017-07-30 09:38:41 -0700

    Cleaned up RSpec duplication for sign up test. Closes #2

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -5,13 +5,8 @@ require 'rails_helper'
 RSpec.feature 'Accounts', type: :feature do
   scenario 'creating an account' do
     visit root_path
-
     click_link 'Create an account'
-    fill_in 'Name', with: 'test'
-    fill_in 'Subdomain', with: 'test'
-    fill_in 'Email', with: 'test@example.com'
-    fill_in 'Password', with: 'password', exact: true
-    fill_in 'Password confirmation', with: 'password'
+    sign_up(name: 'test', subdomain: 'test', email: 'test@example.com', password: 'password')
     perform_enqueued_jobs do
       click_button 'Create Account'
     end
@@ -34,15 +29,19 @@ RSpec.feature 'Accounts', type: :feature do
 
     visit root_path
     click_link 'Create an account'
-    fill_in 'account[name]', with: 'test'
-    fill_in 'Subdomain', with: 'test'
-    fill_in 'Email', with: 'user@example.com'
-    fill_in 'Password', with: 'password', exact: true
-    fill_in 'Password confirmation', with: 'password'
+    sign_up(name: 'test', subdomain: 'test', email: 'user@example.com', password: 'password')
     click_button 'Create Account'
 
     expect(page.current_url).to eq('http://lvh.me/accounts')
     expect(page).to have_content('Sorry, your account could not be created.')
     expect(page).to have_content('Subdomain has already been taken')
+  end
+
+  def sign_up(name:, subdomain:, email:, password:)
+    fill_in 'Name', with: name
+    fill_in 'Subdomain', with: subdomain
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password, exact: true
+    fill_in 'Password confirmation', with: password
   end
 end
